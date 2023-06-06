8f76a03958857494954abadba3572071c663d0b4
Raul <raul.akerman@gmail.com>
2023-06-06 16:52:40 +0300

    Added time field to todos

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,11 @@ app.post('/todos', (req,res) =>{
     if ( !title || !desc ) {
         return res.status(400).send()
     }
-    console.log(req.body.title)
+    console.log(req.body.title + " " + req.body.desc)
     todos.push({
       title,
-      desc
+      desc,
+      time: new Date()
     })
     res.status(201).send()
 })
@@ -28,6 +29,20 @@ app.post('/todos', (req,res) =>{
 app.get('/todos', (req,res) =>{
   return res.status(200).json(todos)
 })
+
+app.delete('/todos', (req,res) =>{
+  const {title} = req.body as Partial<Todo>
+  if ( !title ) {
+      return res.status(400).send()
+  }
+  const index = todos.findIndex((todo) => todo.title === title)
+  if (index === -1) {
+    return res.status(404).send()
+  }
+  todos.splice(index, 1)
+  return res.status(200).send()
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export interface Todo {
     title: string
     desc: string
+    time: Date
   }
